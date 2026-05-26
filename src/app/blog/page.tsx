e3e1f6a19a3bfc6b7d603c34c077240a2220b1d8
tangwei <996501126@qq.com>
2026-05-26 17:53:48 +0800

    修改blog页面

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,14 +1,5 @@
-import {getCloudflareContext} from "@opennextjs/cloudflare";
 
 export default async function Blog() {
-
-    const {env} = getCloudflareContext()
-
-    const stmt = env.test_db.prepare(`SELECT * FROM Customers limit 1`);
-
-    const result = await stmt.all();
-    console.log(result.results,1,2,3)
-
     return (
         <div>
             <div>blog</div>
