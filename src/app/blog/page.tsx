5ad6e7c5915f816cb8e894b00a2adf36764361d4
tangwei <996501126@qq.com>
2026-05-28 10:57:43 +0800

    修改编译错误

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,13 +1,6 @@
 // 后端测试页面 - 演示如何操作数据库
-import {update} from "@/lib/database"
 
 export default async function Blog() {
-
-
-  const result = await update("UPDATE users SET name = ? WHERE id = ?",['更新用户',1]);
-
-  console.log(result);
-
 
 
 	return "blog";
