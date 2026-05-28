e6690108e02b305e71dc6d387614ba84d40a9e2e
tangwei <996501126@qq.com>
2026-05-28 13:52:13 +0800

    upate blog

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,13 +1,7 @@
 // 后端测试页面 - 演示如何操作数据库
 
-import { getCloudflareContext } from '@opennextjs/cloudflare';
 
 export default async function Blog() {
-
-
-const {env} = getCloudflareContext();
-
-console.log(env);
 
 	return "blog";
 }
